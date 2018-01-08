f60cb296c75fb2f5afc0db30ee3a4bfaa6e2e1c7
kitchell <lindseykitchell21@gmail.com>
2018-01-08 17:04:33 -0500

    update

--- laplace_beltrami_spectrum.m
+++ laplace_beltrami_spectrum.m
@@ -4,8 +4,9 @@ function [evecs evals] = laplace_beltrami_spectrum(surf_filename, k)
 
 [V,F] = read_vtk(surf_filename);
 % k = 50;
-L = cotmatrix(V',F'); % some implementation may put L = 0.5*cotmatrix(V,F); this just scales ED by 0.5 and does not really matter.
-M = massmatrix(V',F','barycentric');
+V = normalize_by_SA(V, F);
+L = cotmatrix(V,F'); % some implementation may put L = 0.5*cotmatrix(V,F); this just scales ED by 0.5 and does not really matter.
+M = massmatrix(V,F','barycentric');
 [EV,ED] = eigs(L,M,k,'sm');
 ED = - ED; % minus sign is needed since cotmatrix() gives a negative definite matrix.
 
