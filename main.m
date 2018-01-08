0f9403a24782617303b7bd57dbd12340bd73f7fd
kitchell <lindseykitchell21@gmail.com>
2018-01-08 17:05:57 -0500

    update

--- main.m
+++ main.m
@@ -16,6 +16,7 @@ config = loadjson('config.json');
 
 filelist = dir([config.surfaces '/*.vtk']);
 for file = 2:size(filelist)
+    sprintf(filelist(file).name)
     [evecs, evals] = laplace_beltrami_spectrum([config.surfaces '/' filelist(file).name],config.spectrum_size);
     eval_json.(filelist(file).name(1:end-4)) = evals(:)';
     evecs_json.(filelist(file).name(1:end-4)) = evecs;
