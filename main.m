fdb3a7d56b604979df8f35d82f2984dd9f8889e2
kitchell <lindseykitchell21@gmail.com>
2018-01-08 13:47:02 -0500

    updatae

--- main.m
+++ main.m
@@ -18,7 +18,7 @@ filelist = dir([config.surfaces '/*.vtk']);
 for file = 2:size(filelist)
     [evecs, evals] = laplace_beltrami_spectrum([config.surfaces '/' filelist(file).name],config.spectrum_size);
     eval_json.(filelist(file).name(1:end-4)) = evals(:)';
-    evecs_json.(filelist(file).name(1:end-4)) = evecs(:)';
+    evecs_json.(filelist(file).name(1:end-4)) = evecs;
 end
 
 
